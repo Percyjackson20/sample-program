16e8300051e7ee7c0b249e751fe1d2e5ac47ab97
Percyjackson20 <sathiyavishnu091@gmail.com>
2018-05-29 12:08:41 +0530

    Relational Operators

---
+++ Relational.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Relational
+{
+public static void main(String args[])
+{
+int t1;
+int t2;
+Scanner s=new Scanner(System.in);
+Scanner a=new Scanner(System.in);
+t1=s.nextInt();
+t2=a.nextInt(); 
+System.out.println(t1&t2);
+System.out.println(t1|t2);
+System.out.println(t1^t2);
+}
+}
