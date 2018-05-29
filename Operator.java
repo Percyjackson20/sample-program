25bc3f764eaeef9c5093b83fe4ba9e5256927c80
Percyjackson20 <sathiyavishnu091@gmail.com>
2018-05-29 12:00:45 +0530

    arithematic Operators

---
+++ Operator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Operator
+{
+public static void main(String args[])
+{
+int t1;
+int t2;
+Scanner s=new Scanner(System.in);
+Scanner a=new Scanner(System.in);
+t1=s.nextInt();
+t2=a.nextInt(); 
+System.out.println(t1+t2);
+System.out.println(t1-t2);
+System.out.println(t1*t2);
+System.out.println(t1/t2);
+}
+}
