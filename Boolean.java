f7c9e24bef41d400a974d91dbd20a1f1b773439a
Percyjackson20 <sathiyavishnu091@gmail.com>
2018-05-29 12:22:31 +0530

    Boolean Binary Operator

---
+++ Boolean.java
@@ -0,0 +1,17 @@
+public class Boolean
+{
+public static void main(String a[])
+{
+boolean b=true;
+System.out.println(!b);
+int c=5;
+if((b)&&(c==1))
+{
+System.out.println("Welcome...AND");
+}
+else if((b)||(c==1))
+{
+System.out.println("Welcome...OR");
+}
+}
+}
